6da1c2c3a04a8622301a79c158e62b41919d0148
Rosiflette <janinejacques.tod@gmail.com>
2019-04-06 22:10:31 +0200

    fr

--- MVCBlog/views/pages/saisir.php
+++ MVCBlog/views/pages/saisir.php
@@ -1,7 +1,7 @@
 <p>Saisir un Post</p> <br>
 
 <form class="" action="main.ctrl.php" method="post">
-  <textarea id="post" name="post" rows="5" cols="50" placeholder="Saisissez votre post ici..."></textarea><br>
+  <textarea id="post" name="post" rows="5" cols="50" placeholder="Saisissez votre post ici...">ef</textarea><br>
   <input type="submit" name="action" value="Annuler">
   <input type="submit" name="action" value="Valider">
 </form>
